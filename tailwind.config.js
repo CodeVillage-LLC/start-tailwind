e7ed5b11568ec4c82e3434d66b0f294fda0d84ee
Abdulsamii Ajala <kgasta@gmail.com>
2020-09-02 17:33:23 +0100

    feat: init commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'arokiun-blue': '#007bff',
+        'aro-blue': '#0069d9',
+        grey: '#f8f9fa'
+      },
+      fontSize: {
+        '7xl': '5rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
